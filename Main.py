a4ca4b3a8a93740cabc6dedfe158c3a921b26e1a
Mravacardo <matthewheathcote@icloud.com>
2025-04-02 16:47:55 +0100

    Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import tkinter.messagebox
 import random
 
 root = tkinter.Tk()
-root.minisize(350, 260)
+root.minsize(350, 260)
 root.title('Guess the number game')
 
 number = random.randint(1,20)
@@ -31,3 +31,21 @@ def btn_confirm():
     myName = text_name.get()
     tkinter.messagebox.showinfo("name", 'Well,' + myName + ',I am thinking of a number between 1 and 20.')
 
+label = tkinter.Label(root, text="Welcome to our game!")
+label.pack()
+label_name = tkinter.Label(root, text="What's your name?")
+label_name.place(x=10, y=60)
+text_name = tkinter.Entry(root, width=20)
+text_name.place(x=10, y=90)
+
+btnOK = tkinter.Button(root, text="OK", command=btn_confirm)
+btnOK.place(x=200, y=90, height=28)
+
+label_guess = tkinter.Label(root, text='Take a guess:')
+label_guess.place(x=10, y=150)
+text_guess = tkinter.Entry(root, width=10)
+text_guess.place(x=90, y=150)
+btnCheck = tkinter.Button(root, text='Guess', command=check_num)
+btnCheck.place(x=200, y=150, width=45, height=28)
+
+root.mainloop()
